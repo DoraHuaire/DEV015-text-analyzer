229ade3a47b16ce24d26cba10028a810140e53b7
unknown <dorahuairesolis@gmail.com>
2024-07-26 10:48:24 -0500

    código completo

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,66 @@
 const analyzer = {  
+  //separamos con split, que corta en cada espacio en blanco
+  //\s para espacios en blanco
+  //validamos que no si existen cadenas vacias  no las cuente y retorme 0
+  //.length para que nos devuelva la nueva cantidad de palabras del texto
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const WordCount = text.split(/\s+/g);
+    if (text === "") 
+      return 0;
+    return WordCount.length;
   },
+
+  //solo contamos todo el texto ingresado
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length
   },
+
+  //.trim( ) elimina los espacios en blanco en ambos extremos del string
+  //.replace nos permite reemplazar por el parámetro que le pasemos (espacios en blano, por cadenas vacias)
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const characterCountExcludingSpaces = text.trim().replace(/\s+/g, "");
+    return characterCountExcludingSpaces.length;
   },
+
+  //primero separamos todo el texto unicamente en palabras 
+  //guardamos la longitud de palabras en una variable
+  //.toFixed formatear un número a un número fijo
   getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const word = text.split(/\s+/g);
+    let wordLength = 0;
+    
+    for (let i=0; i<word.length; i++){
+      wordLength = wordLength + word[i].length;
+    }
+
+    const averageWordLength = wordLength / word.length;
+    return averageWordLength.toFixed(2);
+
   },
+
+  //Solo contar números
+  //.match busca coincidencias según la condición que evalue
+  //\d para dígitos, equivalente a [0-9]
+  //validar que se encuentren números
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numberCount = text.match(/\d+/g);
+    if (!numberCount){
+      return 0;
+    } else {
+      return numberCount.length
+    }
   },
+
+  //.reduce() método de arreglo que reduce todos los elementos a un único valor, según dos parámetros
+  //parseInt es una función que comprueba el primer argumento  e intenta devolver un entero de la base especificada. 
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numberSum = text.match(/\d+/g);
+    if (!numberSum){
+      return 0;
+    } 
+    
+    const sum = numberSum.reduce((total, num) => total + parseInt(num, 10), 0);
+    return sum;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const textArea = document.getElementsByName("user-input")[0];
+
+textArea.addEventListener("input", () => {
+  const WordCount = analyzer.getWordCount(textArea.value);
+  document.querySelector(".metricas:nth-child(3)").innerHTML  = "Palabras: " + WordCount;
+  
+  const CharacterCount = analyzer.getCharacterCount(textArea.value);
+  document.querySelector(".metricas:nth-child(1)").innerHTML = "Caracteres: " + CharacterCount;
+
+  const CharacterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(textArea.value);
+  document.querySelector(".metricas:nth-child(2)").innerHTML = "Caracteres sin espacio: " + CharacterCountExcludingSpaces;
+
+  const AverageWordLength = analyzer.getAverageWordLength(textArea.value);
+  document.querySelector(".metricas:nth-child(6)").innerHTML = "Promedio de longitud: " + AverageWordLength;
+
+  const NumberCount = analyzer.getNumberCount(textArea.value);
+  document.querySelector(".metricas:nth-child(4)").innerHTML = "Números: " + NumberCount;
+
+  const NumberSum = analyzer.getNumberSum(textArea.value);
+  document.querySelector(".metricas:nth-child(5)").innerHTML = "Suma de números: " + NumberSum ;
+});
+
+
+//boton para reiniciar//
+const resetButton = document.getElementById("reset-button");
+//evento de escucha para el boton//
+resetButton.addEventListener("click", () => location.reload());
+
